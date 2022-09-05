a0faa42d9a13f27dea79b370938a96111f63cf33
Tomas Thur <thur.tomas@gmail.com>
2022-09-05 11:29:59 +0200

    set the scss files and variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <h1>skuska</h1>
+    <div className="container">
+      
     </div>
   );
 }
