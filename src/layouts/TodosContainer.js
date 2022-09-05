eaba565b59b0464984742406452259b1be8ed6c2
Tomas Thur <thur.tomas@gmail.com>
2022-09-05 15:13:25 +0200

    add delete and finish function

--- src/layouts/TodosContainer.js
+++ src/layouts/TodosContainer.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const TodosContainer = ({todos, setTodos}) => {
+const TodosContainer = ({ todos, setTodos }) => {
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const doneSwitcher = (id) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
-    <div className='todosContainer'>
-        {todos.map(todo => (
-            <h1>{todo.text}</h1>
-        ))}
+    <div className="todosContainer">
+      {todos.map((todo) => (
+        <div className="card">
+          <h3 className={todo.completed ? "green" : ""}>{todo.text}</h3>
+          <button onClick={() => doneSwitcher(todo.id)}>Hotovo!</button>
+          <button onClick={() => deleteTodo(todo.id)}>Vymazat ulohu</button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TodosContainer
+export default TodosContainer;
